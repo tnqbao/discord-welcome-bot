c09903995f56f2c960332a16510eac7dab8e2682
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-07-17 12:04:42 +0700

    set command welcome

--- bot.go
+++ bot.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
+
+var (
+	configMutex sync.Mutex
+	config      *Config
+)
+
+type Config struct {
+	WelcomeChannels map[string]string `json:"welcome_channels"`
+}
+
+func loadConfig() (*Config, error) {
+	file, err := os.Open("config.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &Config{WelcomeChannels: make(map[string]string)}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func saveConfig(config *Config) error {
+	file, err := os.Create("config.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(config)
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,27 +59,43 @@ func main() {
 		log.Fatalf("No token provided. Set DISCORD_BOT_TOKEN environment variable.")
 	}
 
-	channelID := os.Getenv("DISCORD_CHANNEL_ID")
-	if channelID == "" {
-		log.Fatalf("No channel ID provided. Set DISCORD_CHANNEL_ID environment variable.")
-	}
-
-	intents := discordgo.IntentsGuildMembers | discordgo.IntentsGuilds
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalf("Error creating Discord session: %v", err)
 	}
 
-	dg.Identify.Intents = intents
-
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-		memberAdd(s, m, channelID)
+		memberAdd(s, m)
 	})
 	dg.AddHandler(ready)
 
 	err = dg.Open()
 	if err != nil {
 		log.Fatalf("Error opening Discord session: %v", err)
+	}
+
+	config, err = loadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	dg.AddHandler(interactionCreate)
+
+	// Register the /welcome command
+	_, err = dg.ApplicationCommandCreate(dg.State.User.ID, "", &discordgo.ApplicationCommand{
+		Name:        "welcome",
+		Description: "Set the welcome channel",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionChannel,
+				Name:        "channel",
+				Description: "The channel to send welcome messages",
+				Required:    true,
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating command: %v", err)
 	}
 
 	fmt.Println("Bot is now running. Press CTRL+C to exit.")
@@ -51,10 +106,67 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 	fmt.Println("Bot is ready!")
 }
 
-func memberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd, channelID string) {
+func memberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+	configMutex.Lock()
+	defer configMutex.Unlock()
+
+	channelID, ok := config.WelcomeChannels[m.GuildID]
+	if !ok {
+		log.Printf("No welcome channel set for guild: %s", m.GuildID)
+		return
+	}
+
 	message := fmt.Sprintf("Chào mừng <@%s> đến với máy chủ!", m.User.ID)
 	_, err := s.ChannelMessageSend(channelID, message)
 	if err != nil {
 		log.Printf("Error sending message: %v", err)
+	}
+}
+
+func interactionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type != discordgo.InteractionApplicationCommand {
+		return
+	}
+
+	if i.ApplicationCommandData().Name == "welcome" {
+		options := i.ApplicationCommandData().Options
+		var channelID string
+		for _, option := range options {
+			if option.Name == "channel" {
+				channelID = option.ChannelValue(s).ID
+			}
+		}
+
+		if channelID == "" {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Channel ID is required.",
+				},
+			})
+			return
+		}
+
+		configMutex.Lock()
+		config.WelcomeChannels[i.GuildID] = channelID
+		configMutex.Unlock()
+
+		err := saveConfig(config)
+		if err != nil {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Error saving config: %v", err),
+				},
+			})
+			return
+		}
+
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Welcome channel set to <#%s>", channelID),
+			},
+		})
 	}
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.22.5
 
 require (
 	github.com/bwmarrin/discordgo v0.28.1 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
